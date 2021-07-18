9b9752c8e30d11461f63c4bdba98c27bc6f7216b
Dathan Stone <developer.stone@outlook.com>
2021-07-18 00:29:19 -0700

    Added the determineUserLocation method which displays the user's zoneID on a label in the Login Form.

--- src/wgu/stone/controller/LoginController.java
+++ src/wgu/stone/controller/LoginController.java
@@ -3,11 +3,12 @@ package wgu.stone.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import wgu.stone.DAO.UserDAOImpl;
-
 import java.net.URL;
+import java.time.ZoneId;
 import java.util.ResourceBundle;
 
 
@@ -20,18 +21,40 @@ public class LoginController implements Initializable {
     //button for logging in.
     @FXML private Button loginButton;
 
-    //logging into the application. Linked to loginButton
+    //label for user's location
+    @FXML private Label locationLabel;
 
-
-    @FXML
+    /**
+     * Logging into the application. Linked to the loginButton.
+     */
     public void loginToApp() {
             String userName = userNameField.getText();
             String userPassword = passwordField.getText();
             UserDAOImpl.checkUserInfo(userName, userPassword);
     }
 
+    /**
+     * Determines the location of the user and displays it on the locationLabel.
+     */
+    public void determineUserLocation() {
+        locationLabel.setText(ZoneId.systemDefault().toString());
+    }
+
+    /**
+     * Determines the language of the user's computer and sets it for the form.
+     */
+    public void determineUserLanguage() {
+
+    }
+
+    /**
+     * Initializes starting data on the form.
+     * @param url
+     * @param resourceBundle
+     */
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-
+        determineUserLocation();
+        determineUserLanguage();
     }
 }
